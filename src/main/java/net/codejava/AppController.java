ce4f352e94db88911dcdf5cb5558e8942227d7d2
syadla <syadla@SYADLA-L-5212.evoketechnologies.com>
2022-11-06 20:33:53 +0530

    Updated

--- src/main/java/net/codejava/AppController.java
+++ src/main/java/net/codejava/AppController.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class AppController {
 	@Autowired
 	private ProductService service;
